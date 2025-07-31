60d5a72c5bea69b4816e9c47fed420036d210443
Gabriela Araujo <gabyara237@gmail.com>
2025-07-30 23:25:44 -0700

    feat: add Home page component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () =>{
+    return(
+        <div className="home-container">
+            <div className="title-home">
+                <h1>Welcome to Dream Pet Squad</h1>
+            </div>
+            <div className="description-container">
+                <p className="description">Create the pet team of your dreams!<br/> Choose from different types of pets, such as home companions, service dogs, protectors or athletes, and customize their attributes according to their personality, skills and energy. <br/> Start building your Dream Pet Squad and let your love for pets transform into a unique adventure!
+</p>
+            </div>
+
+
+        </div>     
+    )
+};
+
+export default Home;
