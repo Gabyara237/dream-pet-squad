db334205cbeb5a198e2899ae28b012aa5fe29ce3
Gabriela Araujo <gabyara237@gmail.com>
2025-07-30 23:36:29 -0700

    feat: set up routing structure with shared Layoud component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
 
-import './App.css'
 
-function App() {
+const App = () => {
 
 
   return (
-    <>
-      
-       
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
